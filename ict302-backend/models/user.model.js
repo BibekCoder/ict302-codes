d9b0dd251d2208dc6959f2ceaa7dbf7ee8ddc991
CuriousSoul <bibekbamjan405@gmail.com>
2025-12-12 16:38:56 +1100

    Refactor User model for password hashing and verification

--- ict302-backend/models/user.model.js
+++ ict302-backend/models/user.model.js
@@ -1,4 +1,48 @@
+
+const {Model, DataTypes} = require('sequelize');
 const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize) => {
+  class User extends Model {
+    async verifyPassword(plainPassword){
+      return await bcrypt.compare(plainPassword,this.password_hash);
+    }
+  }
+  User.init(
+    {
+      id:{
+        type:DataTypes.UUID,
+        defaultValue:DataTypes.UUIDV4,
+        primaryKey:true,
+      },
+      email:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+        validate:{isEmail:true,},
+      },
+      password_hash:{
+        type:DataTypes.STRING,
+        allowNull:false,
+      },
+      role: {
+        type: DataTypes.ENUM("admin", "support", "founder"),
+        defaultValue: "support",
+      },
+
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      }
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+      timestamps: true,
+    }
+    
+  );
 
 // inside model definition, after define:
 User.beforeCreate(async (user) => {
@@ -7,13 +51,12 @@ User.beforeCreate(async (user) => {
     user.password_hash = hash;
   }
 });
-User.beforeUpdate(async (user) => {
+  User.beforeUpdate(async (user) => {
   if (user.changed('password_hash')) {
     user.password_hash = await bcrypt.hash(user.password_hash, 10);
   }
 });
+return User;
 
-// instance method to verify password
-User.prototype.verifyPassword = async function (plainPassword) {
-  return await bcrypt.compare(plainPassword, this.password_hash);
 };
+
